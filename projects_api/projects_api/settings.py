e0b085b04a1d38e9cf8426b0152b5e77c06c1f5a
Jean TOURE <touremjean@hotmail.com>
2021-03-21 22:51:17 +0000

    Replace SECRET_KEY
    Replace SECRET_KEY raw value by environnment variable.

--- projects_api/projects_api/settings.py
+++ projects_api/projects_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ers*j_zo20gv35#8=7^4dqka-ga6_6asb$nib+d=j=^basuq2l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
